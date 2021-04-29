42fc72e13b0cfa3461a598a14095a85ee322ffb2
17vle3 <17vle3@gmail.com>
2021-04-29 00:55:00 -0700

    slime set up

---
+++ Assets/slimeScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slimeScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //if collider = player
+        //then reduce player's life
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move slime
+    }
+}
