1bd5fcd2c1c6f64968c83c4af3a99835b1ee51f6
17vle3 <17vle3@gmail.com>
2021-04-30 15:57:55 -0700

    portal

---
+++ Assets/Tileset/portal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        /**
+         if(//character entered and press space){
+            scene ++;
+         }
+         * **/
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
