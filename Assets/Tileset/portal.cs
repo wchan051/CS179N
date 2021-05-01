1105337f14d9e71c8ffadb58b000fa95fd9f70bc
17vle3 <17vle3@gmail.com>
2021-05-01 03:32:27 -0700

    portal working

--- Assets/Tileset/portal.cs
+++ Assets/Tileset/portal.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class portal : MonoBehaviour
 {
     public bool touched = false;
-    void OnCollisionEnter(Collision other)
+    public bool spacedown = false;
+    void OnTriggerStay2D(Collider2D other)
     {
-        
-        /**
-         if(other.tag ==player && space down ){
+        //touched = true;
+
+        if (other.gameObject.tag == "Player" && spacedown )
+        { 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            touched = true;
          }
-         * **/
+         
     }
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,13 @@ public class portal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            spacedown = true;
+        }
+        else
+        {
+            spacedown = false;
+        }
     }
 }
