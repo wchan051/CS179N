0310b8eca51adb2ac5aeee9193fddfbe3822f844
17vle3 <17vle3@gmail.com>
2021-05-18 22:02:20 -0700

    back portal

---
+++ Assets/backPortal.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backPortal : MonoBehaviour
+{
+    public bool touched = false;
+    public bool spacedown = false;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //touched = true;
+
+        if (other.gameObject.tag == "Player" && spacedown)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            touched = true;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            spacedown = true;
+        }
+        else
+        {
+            spacedown = false;
+        }
+    }
+}
