a98b7f38d3f3c53268d06d3b9f2d5c4c7c08ffe5
17vle3 <17vle3@gmail.com>
2021-05-20 07:11:44 -0700

    data saves through scenes

---
+++ Assets/capsule.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class capsule : MonoBehaviour
+{
+    GameObject t;
+    public bool typing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GameObject.Find("Text");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        typing = t.GetComponent<storyline1>().typing;
+        if (typing)
+        {
+            GetComponent<SpriteRenderer>().color =  new Color(1f, 1f, 1f, .45f);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 0f);
+        }
+    }
+}
--- Assets/storyline1.cs
+++ Assets/storyline1.cs
@@ -5,53 +5,74 @@ using UnityEngine.UI;
 
 public class storyline1 : MonoBehaviour
 {
+	bool showingText = false;
     string t1 = "ahh... where am i...";
     string t2 = "Wait, didn't I get hit by a car? \nAren't I dead??";
     string t3 = "This place looks a bit familiar. \nIt's almost like I'm in Sycamore Chronicles.";
     string t4 = "holy, is that a slime?? If this is what I think it\nis, I should train to level. Let's try to kill 5 \nslimes.";
 
+	public bool typing = false;
 
 	public float delay = 0.1f;
 	public string fullText;
 	private string currentText = "";
-	 bool done1 = false;
-	 bool done2 = false;
-	 bool done3 = false;
-	public bool done4 = false;
+	 int done1 = 0;
+	 int done2 = 0;
+	 int done3 = 0;
+	 public int done4 = 0;
 
 	// Use this for initialization
 	void Start()
 	{
-		StartCoroutine(ShowText(t1));
+		done1 = PlayerPrefs.GetInt("done1");
+		done2 = PlayerPrefs.GetInt("done2");
+		done3 = PlayerPrefs.GetInt("done3");
+		done4 = PlayerPrefs.GetInt("done4");
+
+		if (done1 == 0) { StartCoroutine(ShowText(t1)); typing = true; }
+		else if (done2 == 0) { StartCoroutine(ShowText(t2)); typing = true; }
+		else if (done3 == 0) { StartCoroutine(ShowText(t3)); typing = true; }
+		else if (done4 == 0) { StartCoroutine(ShowText(t4)); typing = true; }
 	}
 
 	void Update()
 	{
-		/*if (Input.GetKeyDown("space"))
+        /*if (Input.GetKeyDown("space"))
 		{
 			delay = 0.005f;
 		}*/
-		if(done4 && Input.GetKeyDown("space"))
+        if (!showingText) { 
+			if(done4 ==1 && Input.GetKeyDown("space"))
+			{
+				this.GetComponent<Text>().text = "";
+			}
+			else if (done3 == 1 && Input.GetKeyDown("space"))
+			{
+				StartCoroutine(ShowText(t4));
+			}
+			else if (done2 == 1 && Input.GetKeyDown("space"))
+			{
+				StartCoroutine(ShowText(t3));
+			}
+			else if (done1 == 1 && Input.GetKeyDown("space"))
+			{
+				StartCoroutine(ShowText(t2));
+			}
+		}
+
+        if (Input.GetKeyDown(KeyCode.R))
         {
-			this.GetComponent<Text>().text = "";
+			PlayerPrefs.SetInt("done1" ,0);
+			PlayerPrefs.SetInt("done2", 0);
+			PlayerPrefs.SetInt("done3", 0);
+			PlayerPrefs.SetInt("done4", 0);
+
 		}
-		else if (done3 && Input.GetKeyDown("space"))
-		{
-			StartCoroutine(ShowText(t4));
-		}
-		else if (done2 && Input.GetKeyDown("space"))
-		{
-			StartCoroutine(ShowText(t3));
-		}
-		else if (done1 && Input.GetKeyDown("space"))
-        {
-			StartCoroutine(ShowText(t2));
-		}
-		
 	}
 
 	IEnumerator ShowText(string temp)
 	{
+		showingText = true;
 		for (int i = 0; i < temp.Length; i++)
 		{
 			currentText = temp.Substring(0, i);
@@ -59,22 +80,27 @@ public class storyline1 : MonoBehaviour
 			yield return new WaitForSeconds(delay);
 		}
 		delay = 0.025f;
-        if (!done4 && !done3 && !done2 && !done1)
+        if (done4==0 && done3 == 0 && done2 == 0 && done1 == 0)
 		{
-			done1 = true;
-        }
-		else if (!done4 && !done3 && !done2)
-		{
-			done2 = true;
+			done1 = 1;
+			PlayerPrefs.SetInt("done1", 1);
 		}
-		else if (!done4 && !done3)
+		else if (done4 == 0 && done3 == 0 && done2 == 0)
 		{
-			done3 = true;
+			done2 = 1;
+			PlayerPrefs.SetInt("done2", 1);
 		}
-		else if (!done4)
+		else if (done4 == 0 && done3 == 0)
 		{
-			done4 = true;
+			done3 = 1;
+			PlayerPrefs.SetInt("done3", 1);
 		}
-
+		else if (done4 == 0)
+		{
+			done4 = 1;
+			PlayerPrefs.SetInt("done4", 1);
+			typing = false;
+		}
+		showingText = false;
 	}
 }
