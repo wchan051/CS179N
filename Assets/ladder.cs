f2858d6eb9a545fbce6553b17eaf4a7f4851fb6d
17vle3 <17vle3@gmail.com>
2021-05-20 07:50:21 -0700

    ladder working

--- Assets/ladder.cs
+++ Assets/ladder.cs
@@ -6,44 +6,26 @@ using UnityEngine.UI;
 public class ladder : MonoBehaviour
 {
     public float speed;
-    Rigidbody2D rb;
-    float inputHorizontal;
-    float inputVertical;
-    float distance;
-    LayerMask Ladder;
-    bool isClimbing;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void OnTriggerStay2D(Collider2D other)
     {
-        inputHorizontal = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+        if (other.tag == "Player" && Input.GetKey(KeyCode.UpArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
 
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, Ladder);
-        if (hitInfo.collider != null)
-        {   
-            Debug.Log(hitInfo.collider.name);
-            if (Input.GetKeyDown(KeyCode.UpArrow)) {
-                isClimbing = true;
-            }
-            else {
-                isClimbing = false;
-            }
         }
-
-        if (isClimbing) {
-            inputVertical = Input.GetAxisRaw("Vertical");
-            rb.velocity = new Vector2(rb.position.x, inputVertical*speed);
-            rb.gravityScale = 0;
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.DownArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
         }
-        else {
-            rb.gravityScale = 5;
-        }
+        
     }
 }
