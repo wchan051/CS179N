4d5fb09a1cc51f2029de174154783509ad8613ed
joshwu1 <joshwu1229@gmail.com>
2021-06-04 12:52:41 -0700

    pause menu

--- Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
+++ Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
@@ -87,6 +87,7 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
         Debug.Log("Loading Menu..");
 
